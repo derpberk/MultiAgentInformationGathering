d55bdbab2f96fad598213d355865729a2f5ae6fd
samuel <derpberk@gmail.com>
2022-06-06 23:53:48 +0200

    Synchronous and Asynchronous environment added

--- Environment/InformationGatheringEnvironments.py
+++ Environment/InformationGatheringEnvironments.py
@@ -1,46 +1,57 @@
 import gym
+from ray.rllib.env.multi_agent_env import MultiAgentEnv
 import numpy as np
 from sklearn.gaussian_process.kernels import RBF
 from sklearn.gaussian_process import GaussianProcessRegressor
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 
-
-class QuadcopterAgent(gym.Env):
+class QuadcopterAgent:
 
 	_action_types = ['CartesianDiscrete', 'AngleAndDistance']
 
-	def __init__(self,
-	             navigation_map: np.ndarray,
-	             ground_truth_field,
-	             mask_size,
-	             max_distance,
-	             max_illegal_movements,
-	             action_type: dict,
-	             initial_position = None):
+	agent_config_template = {'navigation_map': None,
+	                         'mask_size': None,
+	                         'initial_position': None,
+	                         'speed': None,
+	                         'max_illegal_movements': None,
+	                         'max_distance': None,
+	                         'ground_truth_field': None,
+	                         'dt': None}
+
+	def __init__(self, agent_config: dict, agent_id):
 
 		""" Quadcopter Agent Class:
 		 Serves as an abstract class for the vehicle movement processing.
 		 It defines the individual mesurement process.  """
 
-		self.navigation_map = navigation_map
+		self.agent_id = agent_id
+		self.navigation_map = agent_config['navigation_map']
 
 		self.visitable_positions = np.column_stack(np.where(self.navigation_map == 1))
-		self.initial_position = initial_position
+		self.initial_position = agent_config['initial_position']
 
 		# A dictionary with the action parameters #
-		self.action_type = action_type
 		# Initial position #
 		self.position = None
+		# Waypoints #
+		self.next_wp = None
+		self.director_vector = None
+		self.wp_reached = False
 		# Initial measurement mask #
 		self.measurement = None
-		self.mask_size = mask_size
+		self.mask_size = agent_config['mask_size']
+		self.speed = agent_config['speed']
 
 		# Other variables #
 		self.illegal_movements = 0
-		self.max_illegal_movements = max_illegal_movements
-		self.max_distance = max_distance
+		self.max_illegal_movements = agent_config['max_illegal_movements']
+		self.max_distance = agent_config['max_distance']
 		self.distance = 0
-		self._ground_truth_field = ground_truth_field
+		self._ground_truth_field = agent_config['ground_truth_field']
+		self.dt = agent_config['dt']
+
+		self.fig = None
 
 	@property
 	def ground_truth_field(self):
@@ -50,51 +61,64 @@ class QuadcopterAgent(gym.Env):
 	def ground_truth_field(self, new_value):
 		self._ground_truth_field = new_value
 
-	def step(self, action):
-		""" Process the action given the action type """
+	def go_to_next_waypoint_relative(self, angle, distance):
+		""" Set the next waypoint relative to the current position """
 
-		# A Castesian Discrete [N,S,E,W,NE,NW,SE,SW] movements with fixed distance
-		if self.action_type['type'] == 'CartesianDiscrete':
-			# Cardinal to radians
-			angle = self.action_type['value']/ self.action_type['max'] * 2 * np.pi
-			# Compute next attempted position #
-			next_attempted_position = self.position + self.action_type['max_distance'] * np.asarray([np.cos(angle), np.sin(angle)])
-			# Add the travelled distance
-			self.distance += self.action_space['max_distance']
+		next_attempted_wp = self.position + distance * np.asarray([np.cos(angle), np.sin(angle)])
+		# Check if the next wp is navigable
+		valid = self.check_movement_feasibility(next_attempted_wp)
 
-		# Angle and distance given as actions #
-		elif self.action_type['type'] == 'AngleAndDistance':
+		if not valid:
 
-			# Unpack
-			angle, distance = self.action_type['value']
-			# Compute next attempted position #
-			next_attempted_position = self.position + distance * np.asarray([np.cos(angle), np.sin(angle)])
-			self.distance += distance
+			return 0
 
-		else:
-			raise ValueError("Action type for quadcopter not correct. Choose a correct one")
+			self.wp_reached = True
+			self.next_wp = np.copy(self.position)
 
-		# Check if it is valid (without taking in account other agents)
-		valid = self.check_movement_feasibility(next_attempted_position)
+		self.next_wp = next_attempted_wp
+		self.director_vector = (self.next_wp - self.position)/np.linalg.norm(self.next_wp - self.position)
+		self.wp_reached = False
 
-		if valid:
-			# Update the position #
-			self.position = next_attempted_position
-			# Take a measurement #
-			self.measurement = self.take_measurement()
+		return 1
 
-		else:
-			self.illegal_movements += 1
+	def stop_go_to(self):
+		""" Interrupt the current waypoint """
+		next_attempted_wp = self.position
+		self.wp_reached = True
+
+	def step(self):
+		""" Take 1 step  """
+
+		if not self.wp_reached:
+			# Compute the new position #
+			d_pos = self.speed * self.director_vector * self.dt
+			self.distance += np.linalg.norm(d_pos)
+
+			if self.check_movement_feasibility(self.position + d_pos):
+				# Check if there is any collisions #
+				self.position =  self.position + d_pos
+			else:
+				# If indeed there is a collision, stop and update the measurement #
+				self.illegal_movements += 1
+				self.wp_reached = True
+				self.measurement = self.take_measurement()
+				self.next_wp = np.copy(self.position) # Set the next_wp in the pre-collision position #
+
+			if np.linalg.norm(self.position - self.next_wp) < np.linalg.norm(d_pos) and not self.wp_reached:
+
+				self.wp_reached = True
+				self.next_wp = np.copy(self.position)
+				self.measurement = self.take_measurement()
 
 		done = self.illegal_movements >= self.max_illegal_movements or self.distance > self.max_distance
 
-		return self.measurement, valid, done, {}
+		return {'data': self.measurement, 'position': self.position}, done
 
 	def check_movement_feasibility(self, pos):
 		""" Check if the movement is possible with the current navigation map.
 			This method should be overriden when using the real deploy agent. """
-		visitable = self.navigation_map[pos[0], pos[1]] == 1
-		in_bounds = all(pos >= self.navigation_map.shape) and all(pos < 0)
+		visitable = self.navigation_map[int(pos[0]), int(pos[1])] == 1
+		in_bounds = all(pos <=self.navigation_map.shape) and all(pos > 0)
 
 		return visitable and in_bounds
 
@@ -103,8 +127,10 @@ class QuadcopterAgent(gym.Env):
 		if pos is None:
 			pos = self.position
 
+		pos = pos.astype(int)
+
 		measurement_mask = self._ground_truth_field[pos[0] - self.mask_size[0]: pos[0] + self.mask_size[0] + 1,
-		                                            pos[1] - self.mask_size[1]: pos[0] + self.mask_size[1] + 1]
+		                                            pos[1] - self.mask_size[1]: pos[1] + self.mask_size[1] + 1]
 
 		return measurement_mask
 
@@ -119,54 +145,344 @@ class QuadcopterAgent(gym.Env):
 		self.measurement = self.take_measurement()
 		self.distance = 0
 		self.illegal_movements = 0
+		self.wp_reached = True
+		self.next_wp = np.copy(self.position)
 
-	def render(self, mode='human'):
-		raise NotImplementedError("This method is not implemented yet")
+		return self.measurement
+
+	def render(self):
+
+		if self.fig is None:
+
+			self.fig, self.axs = plt.subplots(1,2)
+
+			self.axs[0].imshow(self.navigation_map, cmap = 'gray')
+			self.d_pos, = self.axs[0].plot(self.position[1], self.position[0], 'rx')
+			self.d_wp, = self.axs[0].plot(self.next_wp[1], self.next_wp[0], 'bo')
+			self.d_sense = self.axs[1].imshow(self.measurement)
+			self.d_mask = self.axs[0].add_patch(Rectangle((self.position[1]-self.mask_size[1]/2, self.position[0]-self.mask_size[0]/2), self.mask_size[1], self.mask_size[0], fc ='g',ec ='g', lw = 2, ))
+
+		else:
+
+			self.d_pos.set_xdata(self.position[1])
+			self.d_pos.set_ydata(self.position[0])
+			self.d_wp.set_xdata(self.next_wp[1])
+			self.d_wp.set_ydata(self.next_wp[0])
+			self.d_sense.set_data(self.measurement)
+			self.d_mask.set_xy((self.position[1]-self.mask_size[1]/2, self.position[0]-self.mask_size[0]/2))
+
+			self.fig.canvas.draw()
 
 
-class MultiAgentOilSpillEnvironment(gym.Env):
+class SynchronousMultiAgentIGEnvironment(MultiAgentEnv):
 
 	def __init__(self, env_config):
+
 		super().__init__()
 
-		# Create N agents #
 		self.env_config = env_config
-		self.agents = [QuadcopterAgent(env_config['agent_config']) for _ in range(env_config['number_of_agents'])]
-		self._agent_ids = tuple([f'agent_{i}' for i in range(env_config['number_of_agents'])])
-		self.dones = [False for _ in range(env_config['number_of_agents'])]
-		self.observation_space = gym.spaces.Box(low=0.0, high=10000, shape=(4, *env_config['navigation_map'].shape))
 
-		# Set the gym.Env mandatory parameters #
-		if env_config['agent_config']['action_type']['type'] == 'CartesianDiscrete':
-			self.action_space = gym.spaces.Discrete(env_config['agent_config']['action_type']['type'])
-		elif env_config['agent_config']['action_type']['type'] == 'AngleAndDistance':
-			self.action_space = gym.spaces.Discrete(env_config['agent_config']['action_type']['type'])
-		else:
-			raise ValueError("Action type for quadcopter not correct. Choose a correct one")
+		# Create the agents #
+		self.agentID = 0
+		self._agent_ids = []
+		self.agents = {}
+		for _ in range(env_config['number_of_agents']):
+			self.spawn_agent()
 
+		self.dones = [False]*env_config['number_of_agents']
+		# List of ready agents #
+		self.agents_ready = [True]*env_config['number_of_agents']
+
+		# Create space-state sets
+		self.observation_space = gym.spaces.Box(low=0.0, high=10000, shape=(5, *env_config['navigation_map'].shape))
+		self.action_space = gym.spaces.Discrete(env_config['number_of_actions'])
+
+		# Other variables
 		self.resetted = False
+		self.measurements = [None] * env_config['number_of_actions']
+		self.valids = [None] * env_config['number_of_actions']
+		self.rewards = {}
+		self.states = {}
 
-		self.states, self.measurements, self.rews, self.dones, self.infos = {}, {}, {}, {}, {}
-
-		""" Estimation related values """
+		""" Regression related values """
 		self.kernel = RBF(length_scale=env_config['kernel_length_scale'])
 		self.GaussianProcess = GaussianProcessRegressor(kernel=self.kernel, optimizer=None)
-		self.mu_model = None
-		self.uncertainty_model = None
+		self.measured_values = None
+		self.measured_locations = None
+		self.mu = None
+		self.uncertainty = None
+		self.uncertainty = None
+		self.Sigma = None
+		self.H_ = 0
+
+	def action2angle(self, action):
+
+		return 2*np.pi * action/self.env_config['number_of_actions']
+
+	def spawn_agent(self):
+
+		agentID = self.agentID
+		self.agents[agentID] = QuadcopterAgent(self.env_config['agent_config'], agentID)
+		self._agent_ids.append(agentID)
+		self.agentID += 1
 
 	def step(self, action_dict):
-		""" Move the agents, take samples and process the new state. """
+		""" Process actions for agents that are available """
 
-		assert self.resetted, "ERROR: The environment must be reseted first!"
+		assert self.resetted, "You need to call env.reset() first!"
 
-		for agent_id, agent in zip(self._agent_ids, self.agents):
-			# Execute every action and get the measurement #
-			self.measurements[agent_id], _, self.dones[agent_id], self.infos[agent_id] = agent.step(action_dict[agent_id])
+		for i, agent in self.agents:
 
-		# Update the model and return the map and the uncertainty #
-		self.mu_model, self.uncertainty_model = self.update_model(self.measurements)
+			action = action_dict[self.agents_ids_dict[i]]
+
+			# Transfor discrete action into an angle
+			ang = self.action2angle(action)
+
+			# Schedule every waypoint
+			agent.go_to_next_waypoint_relative(angle=ang, distance=self.env_config['meas_distance'])
+
+
+		# Move environment until all target positions are reached
+		wp_reached_vec = [False] * self.env_config['number_of_actions']
+		while not all(wp_reached_vec):
+
+			# One step per agent
+			for i in self._agent_ids:
+				meas, ended = self.agent[i].step()
+
+				# Check if reached
+				if self.agents[i].wp_reached:
+					wp_reached_vec[i] = True
+					self.measurements[i] = np.copy(meas) # Copy measurement if so
+				# Check if end of mission
+				if ended:
+					self.dones[i] = True
+
+		# Once every agent has reached its destination, compute the reward and the state #
+
+		self.mu, self.uncertainty, self.Sigma = self.update_model(self.measurements)
+
+		self.rewards = self.reward_function()
+
+		self.states = self.update_states()
+
+		self.dones = {i: self.agents[i].done for i in self._agent_ids}
+
+
+		return self.states, self.rewards, self.dones, {}
+
+	def update_model(self):
+
+		""" Fit the gaussian process using the measurements and
+		 return a new inferred map and its uncertainty """
+
+		# Append the new data #
+		if self.measured_locations is None:
+			self.measured_locations = np.asarray([self.measurements[ind]['position'] for ind in self._agent_ids])
+			self.measured_values = np.asarray([np.mean(self.measurements[ind]['data']) for ind in self._agent_ids])
+		else:
+			self.measured_locations = np.vstack((self.measured_locations, np.asarray([self.measurements[ind]['position'] for ind in self._agent_ids])))
+			self.measured_values = np.vstack((self.measured_values, np.asarray([np.mean(self.measurements[ind]['data']) for ind in self._agent_ids])))
+
+		self.GaussianProcess.fit(self.measured_locations, self.measured_values)
+
+		mu, Sigma = self.GaussianProcess.predict(self.visitable_positions, return_cov=True)
+		uncertainty = Sigma.diagonal()
+
+		mu_map = np.copy(self.env_config['navigation_map'])
+		mu_map[self.visitable_position] = mu
+
+		uncertainty_map = np.copy(self.env_config['navigation_map'])
+		uncertainty_map[self.visitable_position] = uncertainty
+
+		return mu, uncertainty, Sigma
+
+	def reward_function(self):
+
+		Tr = self.Sigma.trace()
+
+		rew =  self.H_ - Tr
+
+		self.H_ = Tr
+
+		return {i: rew for i in self._agent_ids}
+
+	def update_states(self):
+		""" Update the states """
+
+		states = {i: self.individual_state(i) for i in self._agent_ids}
+
+		return states
+
+	def individual_state(self, agent_indx):
+
+		# First channel: navigation/obstacle map
+		nav_map = np.copy(self.env_config['navigation_map'])
+
+		# Second channel: self-position map
+		position_map = np.zeros_like(self.env_config['navigation_map'])
+		agent_position = self.agents[agent_indx].position.astype(int)
+		position_map[agent_position[0], agent_position[1]] = 1.0
+
+		# Third channel: Other agents channel
+		other_agents_map = np.zeros_like(self.env_config['navigation_map'])
+		other_agents_ids = np.copy(self._agent_ids)
+		other_agents_ids.pop(agent_indx)
+
+		for agent_id in other_agents_ids:
+
+			agent_position = self.agents[agent_id].position.astype(int)
+			other_agents_map[agent_position[0], agent_position[1]] = 1.0
+
+
+		return np.concatenate((nav_map[np.newaxis],
+		                       agent_position[np.newaxis],
+		                       other_agents_map[np.newaxis],
+		                       self.mu[np.newaxis],
+		                       self.uncertainty[np.newaxis]))
+
+	def reset(self):
+
+		self.resetted = True
+
+		# Reset the vehicles and take the first measurements #
+		self.measurements = [agent.reset() for agent in self.agents]
+
+		# Update the model with the initial values #
+		self.mu, self.uncertainty, self.Sigma = self.update_model(new_measurements=self.measurements)
+
+		# Update the states
+		self.states = self.update_states()
+
+		return self.states
+
+class AsyncronousMultiAgentIGEnvironment(SincronousMultiAgentIGEnvironment, MultiAgentEnv):
+
+	def __init__(self):
+
+		super(AsyncronousMultiAgentIGEnvironment, self).__init__()
+
+	def action2angdistance(self, action):
+
+		action = (action + 1)/2 # From [-1,1] to [0,1]
+		action[1] = action[1]*2*np.pi
+		action[2] = action[1] * (self.env_config['max_meas_distance'] - self.env_config['min_meas_distance']) + self.env_config['min_meas_distance']
+
+		return action
+
+
+	def step(self, action_dict):
+		""" Process actions for agents that are available """
+
+		assert self.resetted, "You need to call env.reset() first!"
 
 
 
+		for i, action in action_dict.keys():
+
+			# Transfor discrete action into an angle
+			ang = self.action2angdistance(action)
+
+			# Schedule every waypoint
+			self.agent[i].go_to_next_waypoint_relative(angle=ang, distance=self.env_config['meas_distance'])
+
+		wp_reached_vec = [agent.wp_reached for agent in self.agents]
+		while not any(wp_reached_vec):
+
+			# One step per agent
+			for i in self._agent_ids:
+				meas, ended = self.agent[i].step()
+
+				# Check if reached
+				if self.agents[i].wp_reached:
+					wp_reached_vec[i] = True
+					self.measurements[i] = np.copy(meas)  # Copy measurement if so
+				# Check if end of mission
+				if ended:
+					self.dones[i] = True
+
+		# Once every agent has reached its destination, compute the reward and the state #
+
+		self.mu, self.uncertainty, self.Sigma = self.update_model()
+
+		self.rewards = self.reward_function()
+
+		self.states = self.update_states()
+
+		self.dones = {i: self.agents[i].done for i in self._agent_ids}
+
+		return self.states, self.rewards, self.dones, {}
 
 
+
+if __name__ == '__main__':
+
+
+	""" Test for one agent """
+
+	agent_config = {'navigation_map': np.ones((100,100)),
+	                'mask_size': (10,10),
+	                'initial_position': np.array([50,50]),
+	                'speed': 1,
+	                'max_illegal_movements': 10,
+	                'max_distance': 10000000,
+	                'ground_truth_field': np.random.rand(100,100),
+	                'dt': 1}
+
+	agent = QuadcopterAgent(agent_config, 'pepe')
+
+	agent.reset()
+	agent.render()
+
+	def target_to_angledist(current, target):
+
+		diff = target - current
+		angle = np.arctan2(diff[1], diff[0])
+		dist = np.linalg.norm(diff)
+
+		return angle,dist
+
+	def onclick(event):
+
+		global agent
+
+		agent.stop_go_to()
+		new_position = np.asarray([event.ydata, event.xdata])
+		angle,dist = target_to_angledist(agent.position, new_position)
+		agent.go_to_next_waypoint_relative(angle, dist)
+
+
+	cid = agent.fig.canvas.mpl_connect('button_press_event', onclick)
+
+	done = False
+
+	indx = 0
+
+	x,y = np.meshgrid(np.arange(10,80,10), np.arange(10,80,10))
+
+	wps = np.column_stack((y.flatten(),x.flatten()))
+
+	indx = 0
+
+	print("Waypoint ", wps[indx,:])
+	angle, dist = target_to_angledist(agent.position, wps[indx,:])
+	agent.go_to_next_waypoint_relative(angle, dist)
+
+	while True:
+
+
+		_,done = agent.step()
+		agent.render()
+		plt.pause(0.01)
+
+
+		if indx == len(wps) - 1:
+			agent.reset()
+			indx = 0
+
+		if agent.wp_reached:
+
+			indx += 1
+			print("Waypoint ", wps[indx, :])
+			angle, dist = target_to_angledist(agent.position, wps[indx,:])
+			agent.go_to_next_waypoint_relative(angle, dist)
--- Environment/UncertaintyExperiment.py
+++ Environment/UncertaintyExperiment.py
@@ -6,19 +6,32 @@ from deap import benchmarks
 from mpl_toolkits.mplot3d import Axes3D
 
 """ Gaussian Process Regressor """
-gp = GaussianProcessRegressor(kernel=RBF(length_scale=0.08, length_scale_bounds='fixed'), alpha=0.01, n_restarts_optimizer=5)
-A = [[0.5, 0.5], [0.25, 0.25], [0.25, 0.75], [0.75, 0.25], [0.75, 0.75]]
-C = [0.002, 0.5, 0.5, 0.5, 0.5]
+gp = GaussianProcessRegressor(kernel=RBF(length_scale=0.08, length_scale_bounds='fixed'), alpha=0, n_restarts_optimizer=5)
+
+""" Benchmark parameters """
+A = [[0.5,  0.5],
+     [0.25, 0.25],
+     [0.25, 0.75],
+     [0.75, 0.25],
+     [0.75, 0.75]]
+
+C = [0.02, 0.02, 0.02, 0.005, 0.005]
 
 def shekel_arg0(sol):
     return benchmarks.shekel(sol, A, C)[0]
+
+def r_interest(val_mu):
+
+    return np.max((0, 1 + val_mu))
+
 
 """ Compute Ground Truth """
 fig, ax = plt.subplots(1,3)
 X = np.arange(0, 1, 0.02)
 Y = np.arange(0, 1, 0.02)
 X, Y = np.meshgrid(X, Y)
-Z = np.fromiter(map(shekel_arg0, zip(X.flat,Y.flat)), dtype=np.float, count=X.shape[0]*X.shape[1]).reshape(X.shape)*0.02
+Z = np.fromiter(map(shekel_arg0, zip(X.flat,Y.flat)), dtype=float, count=X.shape[0]*X.shape[1]).reshape(X.shape)
+Z = (Z - Z.min())/(Z.max() - Z.min() + 1E-8)
 x_locs = np.column_stack((X.flatten(),Y.flatten()))
 x_meas = np.asarray([[0.0, 0.0]])
 y_meas = np.asarray([Z[int(x_meas[-1, 0]), int(x_meas[-1,1])]])
@@ -26,20 +39,20 @@ y_meas = np.asarray([Z[int(x_meas[-1, 0]), int(x_meas[-1,1])]])
 """ Fit the data """
 gp.fit(x_meas, y_meas)
 mu, sigma = gp.predict(x_locs, return_cov=True)
-print(gp.kernel_)
-mu = mu.reshape(Z.shape)
-H = sigma.trace()/np.sqrt(sigma.shape[0])
-print('Entropy: ', H)
+
+""" Compute the entropy """
+r = np.array(list(map(r_interest, mu.flatten())))
+sigma_weighted = sigma + np.diag(r)
+H1 = sigma_weighted.trace() / np.sqrt(sigma_weighted.shape[0])
+
+print('Entropy: ', np.linalg.det(sigma_weighted))
 
 """ Plot the data"""
-d = ax[0].imshow(Z, cmap='jet')
-ds = ax[1].imshow(sigma.diagonal().reshape(Z.shape), cmap='viridis', vmin=1.2, vmax=3)
+d = ax[0].imshow(mu.reshape(Z.shape), cmap='jet', vmin=0, vmax=1)
+ds = ax[1].imshow(sigma.diagonal().reshape(Z.shape), cmap='viridis')
 d1, = ax[0].plot(x_meas[:,1]*50, x_meas[:,0]*50, 'r-x')
-de = ax[2].imshow(sigma, cmap='viridis')
+de = ax[2].imshow(Z, cmap='viridis')
 
-def r_interest(val_mu):
-
-    return np.min((5, (1.5-1) * val_mu +1))
 
 
 def onclick(event):
@@ -50,25 +63,33 @@ def onclick(event):
     global d
     global H
 
+    """ Get the new measurement """
     x_meas = np.vstack((x_meas, [event.xdata/50, event.ydata/50]))
     y_meas = np.vstack((y_meas, [Z[int(x_meas[-1, 0]*50), int(x_meas[-1, 1]*50)]]))
+
+    un_locs, indxs = np.unique(x_meas, return_index=True, axis=0)
+    un_values = y_meas[indxs]
+
+    """ Fit the GP """
     gp.fit(x_meas, y_meas)
-    print(gp.kernel_)
-    mu_, sigma = gp.predict(x_locs, return_cov=True)
+    mu_, sigma_ = gp.predict(x_locs, return_cov=True)
+
+    """ Compute the entropy """
     r = np.array(list(map(r_interest, mu_.flatten())))
-    sigma = sigma + np.diag(r)
-    sigma = sigma
+    sigma_weighted = sigma_ + np.diag(r)
+
     mu_ = mu_.reshape(Z.shape)
-    H_new = sigma.trace()/np.sqrt(sigma.shape[0])
-    print('Entropy: ',H-H_new)
-    H = H_new
+    H_new = sigma_weighted.trace() / np.sqrt(sigma_weighted.shape[0])
+    print('Entropy: ', H1/H_new)
+
+
+    """ Plot the results """
     d.set_data(mu_)
     d1.set_xdata(x_meas[:,0]*50)
     d1.set_ydata(x_meas[:,1]*50)
-    ds.set_data(sigma.diagonal().reshape(Z.shape))
-    de.set_data(sigma)
+    ds.set_data(sigma_.diagonal().reshape(Z.shape))
+    de.set_data(Z)
     fig.canvas.draw()
-    print(sigma.diagonal())
 
 
 cid = fig.canvas.mpl_connect('button_press_event', onclick)
