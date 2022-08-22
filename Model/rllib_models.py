4e9587441cfe7c0c799f1315ca6ec3510a359f83
Samuel Yanes <45543903+derpberk@users.noreply.github.com>
2022-08-22 15:55:17 +0100

    Fixed GA benchmark

--- Model/rllib_models.py
+++ Model/rllib_models.py
@@ -43,7 +43,6 @@ class FullVisualQModel(TorchModelV2, nn.Module):
 
         return q_vals, state
 
-
     def value_function(self):
         return self.v_vals
 
@@ -99,6 +98,56 @@ class DictVisualQModel(TorchModelV2, nn.Module):
 
     def value_function(self):
         return self.v_vals
+
+class FullVisualActorModel(TorchModelV2, nn.Module):
+
+    def __init__(self, obs_space, action_space, num_outputs, model_config, name):
+        nn.Module.__init__(self)
+        TorchModelV2.__init__(self, obs_space, action_space, num_outputs, model_config, name)
+
+        """ Create a squential model with convolutional neural network with 3 convolutional layers """
+        self.cnn = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels=obs_space.shape[0], out_channels=32, kernel_size=3, stride=1, padding=0),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(in_channels=32, out_channels=16, kernel_size=3, stride=1, padding=0),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(in_channels=16, out_channels=8, kernel_size=3, stride=1, padding=0),
+            torch.nn.ReLU(),
+            torch.nn.Flatten())
+
+        """ Compute the output size of the CNN """
+        cnn_output_size = self.cnn(torch.zeros(1, *obs_space.shape)).shape[1]
+
+        """ Create a sequential model with 3 fully connected layers and ReLu activation function """
+        self.fc = torch.nn.Sequential(
+            torch.nn.Linear(cnn_output_size, 256),
+            torch.nn.ReLU(),
+            torch.nn.Linear(256, 256),
+            torch.nn.ReLU(),
+            torch.nn.Linear(256, num_outputs))
+
+        self.v_vals = None
+
+        self.v_fc = torch.nn.Sequential(
+            torch.nn.Linear(num_outputs, 64),
+            torch.nn.ReLU(),
+            torch.nn.Linear(64, 1),)
+
+    def forward(self, input_dict, state, seq_lens):
+        """ Get the state from input_dict and forward in this order: 1) cnn, 2) fc, 3) q_fc, 4) v_fc """
+        x = input_dict["obs"]
+        x = self.cnn(x)
+        logits = self.fc(x)
+
+        self.v_vals = self.v_fc(logits)  # Save value function for later use
+
+        return logits, state
+
+    def value_function(self):
+
+        return torch.reshape(self.v_vals, [-1])
+
+
 
 if __name__ == '__main__':
     import gym
--- Other Benchmarks/GeneticAlgorithmOffline/GeneticAlgorithmWandering.py
+++ Other Benchmarks/GeneticAlgorithmOffline/GeneticAlgorithmWandering.py
@@ -10,39 +10,42 @@ from StochasticEASimple import eaSimpleWithReevaluation, cxTwoPointCopy
 from Environment.IGEnvironments import InformationGatheringEnv
 from Evaluation.Utils.metrics_wrapper import MetricsDataCreator
 
-
 """ Compute Ground Truth """
 navigation_map = np.genfromtxt('/Users/samuel/MultiAgentInformationGathering/Environment/wesslinger_map.txt')
 N = 4
 same_evaluation_scenario = True
 
 env_config = {
-    'fleet_configuration': {
-        'vehicle_configuration': {
-            'dt': 0.5,
-            'navigation_map': navigation_map,
-            'target_threshold': 0.5,
-            'ground_truth': np.random.rand(50,50),
-            'measurement_size': np.array([2, 2])
-        },
-        'number_of_vehicles': N,
-        'max_distance': 100,
-        'initial_positions': np.array([[15, 19],
-                                        [13, 19],
-                                        [18, 19],
-                                        [15, 22]])
-    },
-    'movement_type': 'DIRECTIONAL',
-    'navigation_map': navigation_map,
-    'dynamic': 'Shekel',
-    'min_measurement_distance': 5,
-    'max_measurement_distance': 10,
-    'measurement_distance': 3,
-    'number_of_actions': 8,
-    'kernel_length_scale': 2,
-    'random_benchmark': same_evaluation_scenario
+	'fleet_configuration': {
+		'vehicle_configuration': {
+			'dt': 0.1,
+			'navigation_map': navigation_map,
+			'target_threshold': 0.5,
+			'ground_truth': np.random.rand(50, 50),
+			'measurement_size': np.array([0, 0]),
+			'max_travel_distance': 150,
+		},
+		'number_of_vehicles': N,
+		'initial_positions': np.array([[15, 19],
+		                               [13, 19],
+		                               [18, 19],
+		                               [15, 22]])
+	},
+	'movement_type': 'DIRECTIONAL',
+	'navigation_map': navigation_map,
+	'dynamic': 'Shekel',
+	'min_measurement_distance': 3,
+	'max_measurement_distance': 6,
+	'measurement_distance': 2,
+	'number_of_actions': 8,
+	'kernel_length_scale': 2,
+	'random_benchmark': True,
+	'observation_type': 'visual',
+	'max_collisions': 10,
+	'eval_mode': True,
+	'seed': 10,
+	'reward_type': 'improvement',
 }
-
 
 N_EVAL = 1
 NUM_OF_INDIVIDUALS = 20
@@ -63,27 +66,28 @@ paths = MetricsDataCreator(metrics_names=['vehicle', 'x', 'y'],
 
 
 def createValidIndividual(creator):
-    """ Create a valid individual. """
+	""" Create a valid individual. """
 
-    env.reset()
-    done_flag = False
-    individual = []
+	env.reset()
+	done_flag = False
+	individual = []
 
-    while not done_flag:
-        actions = []
+	while not done_flag:
+		actions = []
 
-        for veh_id in range(N):
-            mask = env.get_action_mask(ind=veh_id).astype(int)
-            action = np.random.choice(np.arange(0, 8), p=mask / np.sum(mask))
-            actions.append(action)
+		for veh_id in range(N):
+			mask = env.get_action_mask(ind=veh_id).astype(int)
+			action = np.random.choice(np.arange(0, 8), p=mask / np.sum(mask))
+			actions.append(action)
 
-        actions_dict = {i:a for i, a in enumerate(actions)}
-        individual.extend(actions)
-        _, _, done, _ = env.step(actions_dict)
+		actions_dict = {i: a for i, a in enumerate(actions)}
+		individual.extend(actions)
+		_, _, done, _ = env.step(actions_dict)
 
-        done_flag = np.all(list(done.values()))
+		done_flag = np.all(list(done.values()))
 
-    return creator(individual)
+	return creator(individual)
+
 
 # --- Create the Genetic Algorihtm Strategies --- #
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))  # This means we want to maximize
@@ -97,110 +101,109 @@ toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
 
 def evalEnv(ind, local_env):
-    """ Evaluate the environment N_EVAL times. The individual is split into N agents actions
+	""" Evaluate the environment N_EVAL times. The individual is split into N agents actions
     and each agent is evaluated on the environment. """
 
-    individual = np.array(ind)
-    individual = individual.reshape(-1, N)  # Reshape the individual to be N agents x 101 steps
+	individual = np.array(ind)
+	individual = individual.reshape(-1, N)  # Reshape the individual to be N agents x 101 steps
 
-    for _ in range(N_EVAL):
-        # Evaluate the individual N_EVAL times #
-        local_env.reset()
-        R = []
+	for _ in range(N_EVAL):
+		# Evaluate the individual N_EVAL times #
+		local_env.reset()
+		R = []
 
-        for t in range(len(individual)):
-            # Take the actions at instant t and create a dictionary of actions #
-            actions = {i: a for i, a in enumerate(individual[t, :])}
-            _, rewards, dones, _ = local_env.step(actions)
+		for t in range(len(individual)):
+			# Take the actions at instant t and create a dictionary of actions #
+			actions = {i: a for i, a in enumerate(individual[t, :])}
+			_, rewards, dones, _ = local_env.step(actions)
 
-            R.append(np.sum(list(rewards.values())))
+			R.append(np.sum(list(rewards.values())))
 
-    return np.mean(R),
+	return np.mean(R),
+
 
 toolbox.register("evaluate", evalEnv, local_env=env)
 toolbox.register("mate", cxTwoPointCopy)
 toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
 toolbox.register("select", tools.selTournament, tournsize=5)
 
+
 # pool = multiprocessing.Pool()
 # toolbox.register("map", pool.map)
 
 
 def optimize(local_env, save=False):
+	random.seed(64)
 
-    random.seed(64)
+	pop = toolbox.population(n=NUM_OF_INDIVIDUALS)
 
-    pop = toolbox.population(n=NUM_OF_INDIVIDUALS)
+	# Fix the evaluation function with the current environment
+	toolbox.register("evaluate", evalEnv, local_env=local_env)
 
-    # Fix the evaluation function with the current environment
-    toolbox.register("evaluate", evalEnv, local_env=local_env)
+	# Hall of fame for the TOP 5 individuals #
+	hof = tools.HallOfFame(5, similar=np.array_equal)
 
-    # Hall of fame for the TOP 5 individuals #
-    hof = tools.HallOfFame(5, similar=np.array_equal)
+	# Statistics #
+	stats = tools.Statistics(lambda ind: ind.fitness.values)
+	stats.register("avg", np.mean)
+	stats.register("std", np.std)
+	stats.register("min", np.min)
+	stats.register("max", np.max)
 
-    # Statistics #
-    stats = tools.Statistics(lambda ind: ind.fitness.values)
-    stats.register("avg", np.mean)
-    stats.register("std", np.std)
-    stats.register("min", np.min)
-    stats.register("max", np.max)
+	# Algorithm - Simple Evolutionary Algorithm #
+	eaSimpleWithReevaluation(pop, toolbox, cxpb=0.7, mutpb=0.3, ngen=NUM_OF_GENERATIONS, stats=stats, halloffame=hof)
 
-    # Algorithm - Simple Evolutionary Algorithm #
-    eaSimpleWithReevaluation(pop, toolbox, cxpb=0.7, mutpb=0.3, ngen=NUM_OF_GENERATIONS, stats=stats, halloffame=hof)
+	if save:
 
-    if save:
+		with open(f"ga_simple_optimization_result_{datetime.datetime.now().strftime('%Y_%m_%d-%H:%M_%S')}.txt", "w") as solution_file:
 
-        with open(f"ga_simple_optimization_result_{datetime.datetime.now().strftime('%Y_%m_%d-%H:%M_%S')}.txt", "w") as solution_file:
+			solution_file.write("Optimization result for the GA\n")
+			solution_file.write("---------------------------------\n")
+			solution_file.write("---------------------------------\n")
+			solution_file.write("--------- Best Individuals -----------\n")
 
-            solution_file.write("Optimization result for the GA\n")
-            solution_file.write("---------------------------------\n")
-            solution_file.write("---------------------------------\n")
-            solution_file.write("--------- Best Individuals -----------\n")
+			for idx, individual in enumerate(hof):
+				str_data = ','.join(str(i) for i in individual)
+				solution_file.write(f"Individual {idx}: {str_data}\n")
+				solution_file.write(f"Fitness: {individual.fitness.values}\n")
 
-            for idx, individual in enumerate(hof):
-                str_data = ','.join(str(i) for i in individual)
-                solution_file.write(f"Individual {idx}: {str_data}\n")
-                solution_file.write(f"Fitness: {individual.fitness.values}\n")
+			solution_file.close()
 
-            solution_file.close()
-
-    return hof[0]
+	return hof[0]
 
 
 if __name__ == "__main__":
 
-    np.random.seed(0)
+	np.random.seed(0)
 
-    # Initial reset #
+	# Initial reset #
 
-    env.reset()
+	env.reset()
 
-    """ OPTIMIZE THE SCENARIO """
-    best = np.asarray(optimize(env, save=True))
-    individual = np.asarray(best).reshape(-1, N)
+	""" OPTIMIZE THE SCENARIO """
+	best = np.asarray(optimize(env, save=True))
+	individual = np.asarray(best).reshape(-1, N)
 
-    env.eval()
+	env.eval()
 
-    for run in range(N_EVAL):
-        # Evaluate the individual N_EVAL times #
-        env.reset()
-        R = 0
+	for run in range(N_EVAL):
+		# Evaluate the individual N_EVAL times #
+		env.reset()
+		R = 0
 
-        for t in range(len(individual)):
-            # Take the actions at instant t and create a dictionary of actions #
-            actions = {i: a for i, a in enumerate(individual[t, :])}
-            _, rewards, dones, _ = env.step(actions)
+		for t in range(len(individual)):
+			# Take the actions at instant t and create a dictionary of actions #
+			actions = {i: a for i, a in enumerate(individual[t, :])}
+			_, rewards, dones, _ = env.step(actions)
 
-            R += np.sum(list(rewards.values()))
+			R += np.sum(list(rewards.values()))
 
-            # Register positions and metrics #
-            metrics.register_step(run_num=run, step=t, metrics=[R, env.mse])
-            for veh_id, veh in enumerate(env.fleet.vehicles):
-                paths.register_step(run_num=run, step=t, metrics=[veh_id, veh.position[0], veh.position[1]])
-
+			# Register positions and metrics #
+			metrics.register_step(run_num=run, step=t, metrics=[R, env.mse])
+			for veh_id, veh in enumerate(env.fleet.vehicles):
+				paths.register_step(run_num=run, step=t, metrics=[veh_id, veh.position[0], veh.position[1]])
 
 metrics.register_experiment()
 paths.register_experiment()
-
 
 # pool.close()
