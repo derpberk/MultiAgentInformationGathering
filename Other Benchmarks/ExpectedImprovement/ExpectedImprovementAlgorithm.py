1a8e0c783f8ac506c67678ad3efec2b6ac8bb1ca
Samuel <derpberk@gmail.com>
2022-09-07 14:20:41 +0200

    Updated the EI benchmark conditions

--- Other Benchmarks/ExpectedImprovement/ExpectedImprovementAlgorithm.py
+++ Other Benchmarks/ExpectedImprovement/ExpectedImprovementAlgorithm.py
@@ -7,41 +7,46 @@ from Evaluation.Utils.metrics_wrapper import MetricsDataCreator
 from skopt.acquisition import gaussian_ei
 import matplotlib.pyplot as plt
 
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.metrics import mean_squared_error
+
 navigation_map = np.genfromtxt('../../Environment/wesslinger_map.txt')
 
 N = 4
 
+same_evaluation_scenario = False
 env_config = {
-		'fleet_configuration': {
-			'vehicle_configuration': {
-				'dt': 0.1,
-				'navigation_map': navigation_map,
-				'target_threshold': 0.5,
-				'ground_truth': np.random.rand(50, 50),
-				'measurement_size': np.array([0, 0]),
-				'max_travel_distance': 100,
-			},
-			'number_of_vehicles': N,
-			'initial_positions': np.array([[15, 19],
-			                               [13, 19],
-			                               [18, 19],
-			                               [15, 22]])
+	'fleet_configuration': {
+		'vehicle_configuration': {
+			'dt': 0.1,
+			'navigation_map': navigation_map,
+			'target_threshold': 0.5,
+			'ground_truth': np.random.rand(50, 50),
+			'measurement_size': np.array([0, 0]),
+			'max_travel_distance': 50,
 		},
-		'movement_type': 'DIRECTIONAL',
-		'navigation_map': navigation_map,
-		'dynamic': 'Shekel',
-		'min_measurement_distance': 3,
-		'max_measurement_distance': 6,
-		'measurement_distance': 2,
-		'number_of_actions': 8,
-		'kernel_length_scale': 2,
-		'random_benchmark': True,
-		'observation_type': 'visual',
-		'max_collisions': 10,
-		'eval_mode': True,
-		'seed': 10,
-		'reward_type': 'improvement',
-	}
+		'number_of_vehicles': N,
+		'initial_positions': np.array([[15, 19],
+		                               [13, 19],
+		                               [18, 19],
+		                               [15, 22]])
+	},
+	'movement_type': 'DIRECTIONAL',
+	'navigation_map': navigation_map,
+	'dynamic': 'Shekel',
+	'min_measurement_distance': 6,
+	'max_measurement_distance': 3,
+	'measurement_distance': 5,
+	'number_of_actions': 8,
+	'kernel_length_scale': 5,
+	'random_benchmark': True,
+	'observation_type': 'visual',
+	'max_collisions': 10,
+	'eval_mode': True,
+	'seed': 10,
+	'reward_type': 'improvement',
+}
 
 # Create the environment #
 env = InformationGatheringEnv(env_config=env_config)
@@ -49,12 +54,21 @@ env = InformationGatheringEnv(env_config=env_config)
 np.random.seed(0)
 
 # Create the evaluator and pass the metrics #
-evaluator = MetricsDataCreator(metrics_names=['Mean Reward', 'MSE'], algorithm_name='Greedy ExpImp', experiment_name='ExpectedImprovementResults')
+evaluator = MetricsDataCreator(metrics_names=['Mean Reward',
+                                              'Average Uncertainty',
+                                              'Mean regret',
+                                              'Model Error',
+                                              'Peak position error',
+                                              'Peak value error'],
+                               algorithm_name='Greedy ExpImp',
+                               experiment_name='ExpectedImprovementResults')
 
-paths = MetricsDataCreator(metrics_names=['vehicle', 'x', 'y'],
-                           algorithm_name='Greedy ExpImp',
-                           experiment_name='ExpectedImprovementResults_paths',
-                           directory='./')
+paths = MetricsDataCreator(
+							metrics_names=['vehicle', 'x', 'y'],
+							algorithm_name='Greedy ExpImp',
+							experiment_name='ExpectedImprovementResults_paths',
+							directory='./')
+
 
 def predict_best_action(ei_map):
 	""" Compute the actions to move towards the maximum scaling ei_map position """
@@ -65,7 +79,7 @@ def predict_best_action(ei_map):
 		# Compute the position of the highest value of the ei_map that is at a distance env_config['measurement_distance'] form the vehicle current
 		# position #
 
-		look_ahead_distance = env_config['measurement_distance']*3
+		look_ahead_distance = env_config['measurement_distance'] * 3
 		look_ahead_mask = np.zeros_like(ei_map)
 
 		px, py = vehicle.position.astype(int)
@@ -83,14 +97,15 @@ def predict_best_action(ei_map):
 		ei_position = np.unravel_index(ei_index, ei_map.shape)
 
 		# Compute the angle to the highest value of the ei_map #
-		angle = np.arctan2(ei_position[1]-vehicle.position[1], ei_position[0]-vehicle.position[0])
+		angle = np.arctan2(ei_position[1] - vehicle.position[1], ei_position[0] - vehicle.position[0])
 
 		angle = angle + 2 * np.pi if angle < 0 else angle
 
 		# Compute the action to move towards the highest value of the ei_map #
 
-		error = np.abs(angle - np.arange(0, env_config['number_of_actions'])/env_config['number_of_actions'] * 2*np.pi)
+		error = np.abs(angle - 2 * np.pi * np.arange(0, env_config['number_of_actions']) / env_config['number_of_actions'])
 		error[np.logical_not(env.get_action_mask(idx))] = np.inf
+
 
 		action = np.argmin(error)
 		# Set the action #
@@ -105,6 +120,10 @@ d = None
 
 env.eval()
 
+# Complimentary surrogate model
+
+gp = GaussianProcessRegressor(kernel=RBF(length_scale=15.0, length_scale_bounds=(2.0, 100.0)), alpha=0.001)
+
 # Evaluate for 10 scenarios #
 for run in range(30):
 
@@ -118,12 +137,13 @@ for run in range(30):
 
 	print("Run Nº ", run)
 
+
 	while not done_flag:
 
 		# ---- Optimization routine ---- #
 		# 1. Compute the Expected Improvement map #
 		expected_improvement_values = gaussian_ei(env.visitable_positions, model=env.GaussianProcess, xi=0.1)
-		expected_improvement_map[env.visitable_positions[:,0], env.visitable_positions[:,1]] = expected_improvement_values
+		expected_improvement_map[env.visitable_positions[:, 0], env.visitable_positions[:, 1]] = expected_improvement_values
 		# 2. Compute the actions
 		dict_actions = predict_best_action(expected_improvement_map)
 
@@ -134,7 +154,21 @@ for run in range(30):
 		# Check if done #
 		done_flag = any(list(dones_dict.values()))
 
-		metrics = [R, env.mse]
+		# Lets test if there is much more improvement with a complimentary surrogate model
+		gp.fit(env.measured_locations, env.measured_values)
+
+		surr_mu, surr_unc = gp.predict(env.visitable_positions, return_std=True)
+		real_mu = env.ground_truth.ground_truth_field[env.visitable_positions[:,0], env.visitable_positions[:,1]]
+
+		mse = mean_squared_error(y_true=real_mu, y_pred=surr_mu, squared=False)
+
+		metrics = [info['metrics']['accumulated_reward'],
+		           info['metrics']['uncertainty'],
+		           info['metrics']['instant_regret'],
+		           mse,
+		           info['metrics']['peak_location_error'],
+		           info['metrics']['peak_value_error'],
+		           ]
 
 		evaluator.register_step(run_num=run, step=t, metrics=[*metrics])
 		for veh_id, veh in enumerate(env.fleet.vehicles):
@@ -143,8 +177,6 @@ for run in range(30):
 		t += 1
 
 		env.render()
-
-
 
 evaluator.register_experiment()
 paths.register_experiment()
